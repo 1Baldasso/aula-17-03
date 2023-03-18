af657a7b99dc47f5af81dfc7a101e1ccb5a8e856
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu>
2023-03-17 21:44:37 -0300

    Importando funções de CRUD

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import { useState, useEffect } from "react";
-import { db, auth } from "./firebaseConnection";
+import { 
+  db, 
+  auth
+} from "./firebaseConnection";
+import {
+  doc,
+  setDoc,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  updateDoc,
+  onSnapshot
+} from 'firebase/firestore'
 
 export default function App()
 {
-
+  return(
+    <>
+      <p>Olá</p>
+    </>
+  ) 
 }
