41a53ea281ed2949ee34a4a7e1f0b5b29b9a471b
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu>
2023-03-17 21:36:43 -0300

    Iniciando projeto

---
+++ src/firebaseConnection.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDA3PteIAY4T27Q55kHtxciVgp9_PxNOXU",
+  authDomain: "bd-aula-17-03.firebaseapp.com",
+  projectId: "bd-aula-17-03",
+  storageBucket: "bd-aula-17-03.appspot.com",
+  messagingSenderId: "1005956813591",
+  appId: "1:1005956813591:web:ede59def73b65e12f79d52"
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
