02a24c4f7ab304fa92a56cf957c40baec2471194
Lucas Baldasso <engs-lucasbaldasso@camporeal.edu.br>
2023-03-20 19:51:54 -0300

    crud

--- src/App.js
+++ src/App.js
@@ -22,9 +22,89 @@ import {
 
 export default function App()
 {
-  return(
-    <>
-      <p>Olá</p>
-    </>
-  ) 
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+  const[idPost , setIdPost] = useState('');
+  function throwError(error){console.log("Erro:",error)};
+  function clearFields(){
+    setAutor('');
+    setTitulo('');
+    setIdPost('');
+  }
+  const [posts, setPosts] = useState([]);
+  useEffect( () => {
+    async function loadPosts(){
+      const dados = onSnapshot(collection(db,"posts"),(snapshot) => {
+        let listaPost = [];
+
+        snapshot.forEach((doc) => {
+          listaPost.push({
+            id: doc.id,
+            titulo: doc.data().titulo,
+            autor: doc.data().autor,
+          })
+        })
+        setPosts(listaPost);
+
+      })
+    }
+
+    loadPosts();
+  }, []);
+
+  async function searchPost()
+  {
+    const postRef = collection(db,"posts");
+    await getDocs(postRef).then((snapshot)=>{
+      let lista = []
+      snapshot.forEach(item => {
+        lista.push({
+          id: item.id,
+          titulo: item.data().titulo,
+          autor: item.data().autor
+        });
+      });
+      setPosts(lista);
+    }).catch((error)=>{
+      throwError(error);
+    });
+  }
+
+  async function addPost()
+  {
+    await addDoc(collection(db,"posts"),{
+      titulo: titulo,
+      autor: autor
+    }).then(()=>{
+      console.log("Cadastro Realizado com sucesso!");
+      clearFields();
+    }
+    ).catch((error)=>{
+      throwError(error);
+    })
+  }
+
+  async function editPost()
+  {
+    const docRef = doc(db,"posts",idPost);
+    await updateDoc(docref,{
+      titulo: titulo,
+      autor: autor
+    }).then(()=>{
+      console.log("POST ATUALIZADO");
+      clearFields();
+    }).catch((error)=>{
+      throwError(error);
+    })
+  }
+
+  async function deletePost(id){
+    const docRef = doc(db,'posts',id);
+    await deletePost(docRef).then(()=>{
+      console.log("Post deletado com sucesso!");
+      clearFields();
+    }).catch((error)=>{
+      throwError(error);
+    })
+  }
 }
